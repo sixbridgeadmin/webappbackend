0dc2b3fba831a3d77c41946ee7764135ddf798a5
lastrasebastian180@gmail.com <lastrasebastian180@gmail.com>
2025-07-03 17:01:58 -0400

    hotfix correo 1.4

--- config/configemail.js
+++ config/configemail.js
@@ -14,14 +14,14 @@
 
 module.exports = {
   email: {
-    host: process.env.EMAIL_SERVICE,
-    port: process.env.EMAIL_PORT,
-    secure: false,
+    host: process.env.EMAIL_HOST || "cp7101.webempresa.eu", // Host de Webempresa
+    port: process.env.EMAIL_PORT || 465, // Puerto seguro SSL
+    secure: true, // true para puerto 465 (SSL)
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.EMAIL_USER || "contacto@sixbridge.cl", // Tu usuario real
+      pass: process.env.EMAIL_PASSWORD || "Sixbridge2025.", // Tu contraseña real
     },
-    from: process.env.EMAIL_FROM,
+    from: process.env.EMAIL_FROM || "contacto@sixbridge.cl",
   },
 };
 
