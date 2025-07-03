4a73911225cac4fb63c86492823af2462184d9dc
lastrasebastian180@gmail.com <lastrasebastian180@gmail.com>
2025-07-03 18:49:50 -0400

    1.7

--- config/configemail.js
+++ config/configemail.js
@@ -1,12 +1,25 @@
 // backend/config.js
+/*module.exports = {
+  email: {
+    host: process.env.EMAIL_HOST || "cp7101.webempresa.eu", // Nuevo
+    port: process.env.EMAIL_PORT || 465, // Nuevo
+    secure: true, // Para puerto 465
+    auth: {
+      user: process.env.EMAIL_USER || "contacto@sixbridge.cl", // Reemplaza esto
+      pass: process.env.EMAIL_PASSWORD || "Sixbridge2025.", // Reemplaza esto
+    },
+    from: process.env.EMAIL_FROM || "contacto@sixbridge.cl",
+  },
+};*/
+
 module.exports = {
   email: {
     host: process.env.EMAIL_HOST || "cp7101.webempresa.eu", // Host de Webempresa
     port: process.env.EMAIL_PORT || 465, // Puerto seguro SSL
     secure: true, // true para puerto 465 (SSL)
     auth: {
-      user: process.env.EMAIL_USER || "contacto@sixbridge.cl", // Solo variable de entorno
-      pass: process.env.EMAIL_PASSWORD || "Sixbridge_2025"// Solo variable de entorno
+      user: process.env.EMAIL_USER || "contacto@sixbridge.cl", // Tu usuario real
+      pass: process.env.EMAIL_PASSWORD || "Sixbridge_2025" // Tu contraseña real
     },
     from: process.env.EMAIL_FROM || "contacto@sixbridge.cl",
   },
